3d63057b714eb037be48d34a069f0c38f09d2a21
Sam Hamilton <samilton@gmail.com>
2024-08-12 21:46:57 -0400

    last try for tonight

---
+++ modules/nixos/roles/desktop/default.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  config,
+  ...
+}:
+with lib;
+with lib.nixicle; let
+  cfg = config.roles.desktop;
+in {
+  options.roles.desktop = {
+    enable = mkEnableOption "Enable desktop configuration";
+  };
+
+  config = mkIf cfg.enable {
+    boot.binfmt.emulatedSystems = ["aarch64-linux"];
+
+    roles = {
+      common.enable = true;
+
+      desktop.addons = {
+        nautilus.enable = true;
+      };
+    };
+
+    hardware = {
+      audio.enable = true;
+      bluetooth.enable = true;
+      logitechMouse.enable = true;
+      zsa.enable = true;
+    };
+
+    services = {
+      nixicle.avahi.enable = true;
+      backup.enable = true;
+      vpn.enable = true;
+      virtualisation.podman.enable = true;
+    };
+
+    system = {
+      boot.plymouth = true;
+    };
+
+    cli.programs = {
+      nh.enable = true;
+      nix-ld.enable = true;
+    };
+
+    user = {
+      name = "haseeb";
+      initialPassword = "1";
+    };
+  };
+}
