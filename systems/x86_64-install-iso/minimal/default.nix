a9ebefdf0eb49368c56cc97b40d687c7e61009a3
Sam Hamilton <samilton@gmail.com>
2024-08-12 22:47:50 -0400

    installer

---
+++ systems/x86_64-install-iso/minimal/default.nix
@@ -0,0 +1,24 @@
+{lib, ...}: {
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.wireless.enable = lib.mkForce false;
+  networking.networkmanager.enable = true;
+
+  nix.enable = true;
+  services = {
+    openssh.enable = true;
+  };
+
+  system = {
+    fonts.enable = true;
+    locale.enable = true;
+  };
+
+  user = {
+    name = "nixos";
+    initialPassword = "1";
+  };
+
+  system.stateVersion = "23.11";
+}
