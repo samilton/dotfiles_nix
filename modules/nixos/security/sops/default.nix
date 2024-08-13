7e549795107cfc778d6a9434d551f17f8e3d42ab
Sam Hamilton <samilton@gmail.com>
2024-08-12 22:21:09 -0400

    last try for tonight

---
+++ modules/nixos/security/sops/default.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib;
+with lib.nixicle; let
+  cfg = config.security.sops;
+in {
+  options.security.sops = with types; {
+    enable = mkBoolOpt false "Whether to enable sop for secrets management.";
+  };
+
+  config = mkIf cfg.enable {
+    sops = {
+      age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+    };
+  };
+}
